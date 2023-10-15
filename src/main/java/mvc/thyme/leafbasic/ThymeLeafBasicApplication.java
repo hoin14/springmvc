e3a138329df1a587d211eadbff2551b4dcb551a5
hoin14 <hoins@localhost>
2023-10-15 22:54:32 +0900

    commit

---
+++ src/main/java/mvc/thyme/leafbasic/ThymeLeafBasicApplication.java
@@ -0,0 +1,13 @@
+package mvc.thyme.leafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mvc/thyme/leafbasic/ThymeLeafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.thyme.leafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
